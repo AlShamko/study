8d235afdf3d181699e3a31084d26f71075db1e9a
AlShamko <shamkoag@gmail.com>
2024-05-17 12:51:08 +0200

    add new function: findSum

---
+++ js/findSum.js
@@ -0,0 +1,12 @@
+let myArray = [1, 2, 3, 4, 5, 6, 7,];
+
+function findSum (array) {
+	let sum = 0;
+
+	for(let i = 0; i< array.length; i++) {
+		sum += array[i] // sum = sum + array[i]
+	}
+	console.log(`sum = ${sum}`)
+}
+
+findSum (myArray)
